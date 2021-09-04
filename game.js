6b9cbd395bd0257d8a9394364beab79a1683c1d3
Packm4n <48672007+Zaykenov@users.noreply.github.com>
2021-09-04 19:53:38 +0600

    Update game.js

--- game.js
+++ game.js
@@ -7,6 +7,7 @@ function nextSequence(){
     gamePattern.push(randomChosenColour);
     $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
     var audio = new Audio("sounds/"+randomChosenColour+".mp3");
+    audio.play();
 }
 
 nextSequence();
