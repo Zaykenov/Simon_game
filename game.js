d0b7f77934ced485bf91dbfdf504dce7b6f8aa9d
Packm4n <48672007+Zaykenov@users.noreply.github.com>
2021-09-05 00:30:02 +0600

    Add files via upload

--- game.js
+++ game.js
@@ -1,21 +1,32 @@
-var gamePattern=[];
+var gamePattern=[];//Gaming set
 var buttonColours=["red","blue","green","yellow"];
-var userClickedPattern=[];
+var userClickedPattern=[];//User response to the game pattern
 
+//Creation of function that will respond to the new tasks
 function nextSequence(){
+    //Chose of a random button 
+    //Adding this random element to the array
     var randomNumber=Math.floor(Math.random()*4);
     var randomChosenColour=buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+    //Creation of animation
     $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    var audio = new Audio("sounds/"+randomChosenColour+".mp3");
-    return audio.play();
+    //Sound of initial element
+    playSound(randomChosenColour);
 }
 
-$(".btn").click(handler);
-
-function handler(){
-    var userChosenColour=$("this").attr("id");
+$(".btn").click(function(){
+    //Adding id of pressed button to the array
+    var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
+    //Sound for pressed buttons
+    playSound(userChosenColour);
+    });
+
+
+function playSound(name){
+    var audio = new Audio("sounds/"+name+".mp3");
+    audio.play();
 }
 
 
